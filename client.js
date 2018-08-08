107c3d7dc3dd80150b2b4e2ec663843ddff14845
May Chen <maychen@Mays-MacBook-Pro.local>
2018-08-07 21:54:48 -1000

    Removed disconnection code, was not working properly.

--- client.js
+++ client.js
@@ -11,10 +11,5 @@ const client = net.createConnection(6969, "0.0.0.0", () => {
     console.log(data.toString());
   });
 
-  client.on("end", () => {
-    console.log("User has disconnected from the server.");
-  })
-
   process.stdin.pipe(client);
 })
-// console.log(client);
