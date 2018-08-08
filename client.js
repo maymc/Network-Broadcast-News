a78074f0659307abd6b5a6fd80c88c487e9e6b53
May Chen <maychen@Mays-MacBook-Pro.local>
2018-08-07 20:30:39 -1000

    Added a disconnection message

--- client.js
+++ client.js
@@ -2,10 +2,19 @@ const net = require("net");
 
 //client is a socket connect
 const client = net.createConnection(6969, "0.0.0.0", () => {
+
+  //"Connect" listener
   client.write("New user has entered. \n");
+
+  //Data from client
   client.on("data", data => {
     console.log(data.toString());
   });
+
+  client.on("end", () => {
+    console.log("User has disconnected from the server.");
+  })
+
   process.stdin.pipe(client);
 })
 // console.log(client);
