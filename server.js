07b9da96b5c55cae389a6e63b2aeb972d6cb3b98
May Chen <maychen@Mays-MacBook-Pro.local>
2018-08-07 21:55:45 -1000

    Added one attack to the game

--- server.js
+++ server.js
@@ -22,24 +22,33 @@ const server = net.createServer(client => {
 
     //Helper
     if (clientMsg.includes("/Help")) {
-      client.write("~~HELP~~\n")
+      client.write("~~HELP~~\n");
+      client.write("'/characterName /attack' -- To attack another character");
     }
 
+    //If user tries to be called an admin
     if (clientMsg.includes("Admin") || clientMsg.includes("ADMIN") || clientMsg.includes("admin")) {
       console.log("[ADMIN] ERROR - Your username cannot be admin. Please choose a different name.");
     }
 
+    //Set up the character once user selects one
     if (clientMsg.includes("Pikachu") || clientMsg.includes("Charmander") || clientMsg.includes("Bulbasaur") || clientMsg.includes("Squirtle")) {
-      //Set user's character
-      client.name = clientMsg;
-      client.write("--> [ADMIN]: Welcome " + client.name);
-      client.hp = 1000;
-      client.mp = 1000;
-      client.write("Here's your stats: [HP: " + client.hp + "] [MP: " + client.mp + "]");
+
+      if (!clientMsg.includes("/thunderbolt")) {
+        //Set user's character
+        client.name = clientMsg;
+        client.write("--> [ADMIN]: Welcome " + client.name);
+        client.hp = 1000;
+        client.mp = 1000;
+        client.write("Here's your stats: [HP: " + client.hp + "] [MP: " + client.mp + "]");
+        clients.push(client);
+      }
     }
+    //If user does not select a character, prompt them to select one again
     else if (client.name === undefined && !clientMsg.includes("Pikachu") && !clientMsg.includes("Charmander") && !clientMsg.includes("Bulbasaur") && !clientMsg.includes("Squirtle")) {
       client.write("\nPlease pick a character to start.\n /Pikachu \n /Charmander \n /Bulbasaur \n /Squirtle\n");
     }
+    //Else, if client selected a character, broadcast the client's message to all other clients
     else {
       clients.forEach(socket => {
         //If not equal to anyone in the socket then it will broadcast to everyone in the socket
@@ -48,10 +57,40 @@ const server = net.createServer(client => {
         }
       })
     }
+
+    //If user attacks another user
+    if (clientMsg.includes("/thunderbolt")) {
+      const thunderbolt = 25;
+      if (clientMsg.includes("/Charmander")) {
+        let charmanderObj = clients.find(obj => {
+          return obj.name === "Charmander\n";
+        });
+        console.log("charmanderObj: ", charmanderObj);
+        charmanderObj.hp -= thunderbolt;
+        client.write("\n>>>Charmander received damage!!<<<\nCurrent Stats:\n[HP: " + charmanderObj.hp + "] [MP: " + charmanderObj.mp + "]");
+      }
+      if (clientMsg.includes("/Squirtle")) {
+        let squirtleObj = clients.find(obj => {
+          return obj.name === "Squirtle\n";
+        });
+        console.log("SquirtleObj: ", squirtleObj);
+        squirtleObj.hp -= thunderbolt;
+        client.write("\n>>>Squirtle received damage!!<<<\nCurrent Stats:\n[HP: " + squirtleObj.hp + "] [MP: " + squirtleObj.mp + "]");
+      }
+      if (clientMsg.includes("/Bulbasaur")) {
+        let bulbasaurObj = clients.find(obj => {
+          return obj.name === "Bulbasaur\n";
+        });
+        console.log("charmanderObj: ", bulbasaurObj);
+        bulbasaurObj.hp -= thunderbolt;
+        client.write("\n>>>Bulbasaur received damage!!<<<\nCurrent Stats:\n[HP: " + bulbasaurObj.hp + "] [MP: " + bulbasaurObj.mp + "]");
+      }
+    }
   });
 
-  clients.push(client);
-  // console.log(clients);
+  client.on("end", () => {
+    console.log(client.name + "-->has disconnected from the server :(.");
+  })
 
   //Admin messages
   process.stdin.setEncoding('utf8');
